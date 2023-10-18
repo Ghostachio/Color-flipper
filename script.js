34141ae70f2b0179d3d58e64ae3035d777e13aca
Ghostachio <mansour.hamza2.0@gmail.com>
2023-10-18 21:06:46 +0100

    modified general variables with object accessing them with destructuring

--- script.js
+++ script.js
@@ -5,9 +5,14 @@ let main = document.getElementById('main');
 
 
 const changeColor = () =>{
-  let randomOne = Math.floor(Math.random() * 256);
-  let randomTwo = Math.floor(Math.random() * 256);
-  let randomThree = Math.floor(Math.random() * 256);
+  let colorPalette  = { 
+    randomOne : Math.floor(Math.random() * 256),
+    randomTwo : Math.floor(Math.random() * 256),
+    randomThree : Math.floor(Math.random() * 256),
+  }
+  
+  let {randomOne, randomTwo, randomThree} = colorPalette;
+
   main.style.backgroundColor = `rgb(${randomOne},${randomTwo},${randomThree})`;
 };
 
